abd99b7966a1aa3befa6c28704b84ae95b2c8d32
Ali Raza <alirazabinhussain@gmail.com>
2024-04-28 15:27:37 +0500

    Counter Project using React useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const addCounter = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+      setErrorMessage(''); 
+    } else {
+      setErrorMessage('Cannot exceed limit of 20');
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 1000);
+    }
+  };
+
+  const removeCounter = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+      setErrorMessage(''); 
+    } else {
+      setErrorMessage('Cannot go below limit of 0');
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 1000);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>React Counter Project</header>
+      <button onClick={addCounter}>Add</button>
+      <input type="text" value={counter} readOnly />      
+      <button onClick={removeCounter}>Remove</button>
+      {errorMessage && (
+        <p className="error">{errorMessage}</p>
+      )}
     </div>
   );
 }
