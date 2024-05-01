62dac3eecb9258d9a8e8640bfa34ac7fe5bcc9f5
Ali Raza <alirazabinhussain@gmail.com>
2024-05-01 14:36:25 +0500

    Code for Background Changer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("white");
+  const [prevColor, setPrevColor] = useState("black");
+
+  const handleColorChange = (newColor) => {
+    setPrevColor(color);
+    setColor(newColor);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <h1 className="text-3xl font-bold underline" style={{ color: prevColor }}>
+        Background Changer - useState & Tailwind CSS
+      </h1>
+      <div className="fixed flex flex-wrap justify-center top-12 inset-x-0 px=2">
+        <div className="fixed flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-lg">
+          <button
+            onClick={() => handleColorChange("red")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg"
+            style={{ backgroundColor: "red", color: "white" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => handleColorChange("green")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg"
+            style={{ backgroundColor: "green", color: "white" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => handleColorChange("orange")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg"
+            style={{ backgroundColor: "orange", color: "white" }}
+          >
+            Orange
+          </button>
+          <button
+            onClick={() => handleColorChange("grey")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg"
+            style={{ backgroundColor: "grey", color: "white" }}
+          >
+            Grey
+          </button>
+          <button
+            onClick={() => handleColorChange("purple")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg"
+            style={{ backgroundColor: "purple", color: "white" }}
+          >
+            Purple
+          </button>
+          <button
+            onClick={() => handleColorChange("black")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg"
+            style={{ backgroundColor: "black", color: "white" }}
+          >
+            Black
+          </button>
+          <button
+            onClick={() => handleColorChange("pink")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg"
+            style={{ backgroundColor: "pink", color: "white" }}
+          >
+            Pink
+          </button>
+          <button
+            onClick={() => handleColorChange("blue")}
+            className="outline-none px-4 py-1 rounded-full shadow-lg"
+            style={{ backgroundColor: "blue", color: "white" }}
+          >
+            Blue
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
